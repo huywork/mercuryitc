5e4212b7f54953aec0de2bf4380c2f7bb6125de1
Huy Duong <huy.duong@diamond.ac.uk>
2026-09-04 10:59:05 +0100

    [I11-910] Removed file created by mistake

--- etc/builder copy.py
+++
@@ -1,9 +0,0 @@
-from iocbuilder import AutoSubstitution
-from iocbuilder.modules.streamDevice import AutoProtocol
-
-class mercuryitc(AutoSubstitution, AutoProtocol):
-    # Substitution attributes
-    TemplateFile = 'mercuryitc.template'
-
-    # AutoProtocol attributes
-    ProtocolFiles = ['mercuryitc.proto']
