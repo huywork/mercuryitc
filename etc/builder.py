d2b18bf9a4965acb7377269e8d265f437a5d4430
Huy Duong <huy.duong@diamond.ac.uk>
2026-09-04 11:00:00 +0100

    [I11-910] Add conents for builder.py

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,11 @@
 
 
+from iocbuilder import AutoSubstitution
+from iocbuilder.modules.streamDevice import AutoProtocol
+
+class mercuryitc(AutoSubstitution, AutoProtocol):
+    # Substitution attributes
+    TemplateFile = 'mercuryitc.template'
+
+    # AutoProtocol attributes
+    ProtocolFiles = ['mercuryitc.proto']
