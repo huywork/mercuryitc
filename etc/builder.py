baf1f72486024f72b42cf10ae1ea2c9a717a0818
Huy Duong <huy.duong@diamond.ac.uk>
2026-09-18 11:18:01 +0100

    [I11-910] Try using global template instead of temperature

--- etc/builder.py
+++ etc/builder.py
@@ -1,9 +1,18 @@
 from iocbuilder import AutoSubstitution
 from iocbuilder.modules.streamDevice import AutoProtocol
 
+class MercuryGlobal(AutoSubstitution, AutoProtocol):
+    # Substitution attributes
+    TemplateFile = 'MercuryGlobal.template'
+
+    # AutoProtocol attributes
+    ProtocolFiles = ['mercuryitc.proto']
+
+'''
 class MercuryTemp(AutoSubstitution, AutoProtocol):
     # Substitution attributes
     TemplateFile = 'MercuryTemp.template'
 
     # AutoProtocol attributes
     ProtocolFiles = ['mercuryitc.proto']
+'''
