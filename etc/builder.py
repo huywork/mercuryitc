bd78c19029752faa7a2f62f32f6dfc2eaa56605d
Huy Duong <huy.duong@diamond.ac.uk>
2026-09-21 09:25:57 +0100

    [I11-910] First draft adding all templates

--- etc/builder.py
+++ etc/builder.py
@@ -1,6 +1,13 @@
 from iocbuilder import AutoSubstitution
 from iocbuilder.modules.streamDevice import AutoProtocol
 
+class MercuryFlow(AutoSubstitution, AutoProtocol):
+    # Substitution attributes
+    TemplateFile = 'MercuryFlow.template'
+
+    # AutoProtocol attributes
+    ProtocolFiles = ['mercuryitc.proto']
+
 class MercuryGlobal(AutoSubstitution, AutoProtocol):
     # Substitution attributes
     TemplateFile = 'MercuryGlobal.template'
@@ -8,11 +15,37 @@ class MercuryGlobal(AutoSubstitution, AutoProtocol):
     # AutoProtocol attributes
     ProtocolFiles = ['mercuryitc.proto']
 
-'''
+class MercuryLevel(AutoSubstitution, AutoProtocol):
+    # Substitution attributes
+    TemplateFile = 'MercuryLevel.template'
+
+    # AutoProtocol attributes
+    ProtocolFiles = ['mercuryitc.proto']
+
+class MercuryPressure(AutoSubstitution, AutoProtocol):
+    # Substitution attributes
+    TemplateFile = 'MercuryPressure.template'
+
+    # AutoProtocol attributes
+    ProtocolFiles = ['mercuryitc.proto']
+
+class MercurySPCAvailable(AutoSubstitution, AutoProtocol):
+    # Substitution attributes
+    TemplateFile = 'MercurySPCAvailable.template'
+
+    # AutoProtocol attributes
+    ProtocolFiles = ['mercuryitc.proto']
+
 class MercuryTemp(AutoSubstitution, AutoProtocol):
     # Substitution attributes
     TemplateFile = 'MercuryTemp.template'
 
     # AutoProtocol attributes
     ProtocolFiles = ['mercuryitc.proto']
-'''
+
+class MercuryVTISPC(AutoSubstitution, AutoProtocol):
+    # Substitution attributes
+    TemplateFile = 'MercuryVTISPC.template'
+
+    # AutoProtocol attributes
+    ProtocolFiles = ['mercuryitc.proto']
